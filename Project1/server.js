0c5b0372ac4929d80d5f0a77a4ba33deab82affa
Your Name <you@example.com>
2023-07-22 15:14:53 +0100

    Rework

--- Project1/server.js
+++ Project1/server.js
@@ -1,56 +1,32 @@
-let express = require("express");
-let server = express();
-let mysql = require("mysql");
+let Express = require("express")
+let server = Express()
+let MySql = require("mysql")
 
-let db = mysql.createConnection({
-    host:"localhost",
+server.set("view engine", "ejs")
+
+let db = MySql.createConnection({
+    host: "localhost",
     user:"root",
     password:"root",
-    database:"nbs2"
-});
+    database: "nbs2"
+})
+db.connect()
 
-db.connect();
-
-server.get("/", function(req,res){
-    res.render("homepage.ejs");
+server.get("/", (req,res)=>{
+    res.render("index.ejs")
     res.end()
 })
 
-server.get("/allemployees", function(req,res){
-    db.query("select * from employees", function(err, data){
-    res.render("employeelist.ejs", {employees:data});
-    res.end()
-   })
-})
-
-server.get("/departmentEmployees/:dept", function(req,res){
-    let sql = "select * from employees where department='"+req.params.dept+"'"
-    db.query(sql, function(err, data){
-    res.render("employeelist.ejs", {employees:data});
-    res.end()
-   })
-})
-
-server.get("/departments", function(req,res){
-    db.query("select distinct department from employees", function(err, data){
-    res.render("departments.ejs", {deptName:data});
-    res.end()
+server.get("/departments", (req,res)=>{
+    
+    db.query("Select distinct department from employee",(err, data)=>{
+        res.render("departments.ejs")
+        res.end()
     })
+
+    
 })
-
-server.get("/deleteEmployee/:empno", function(req,res){
-    let deleteSQL="delete from employees where empno="+req.params.empno
-    db.query(deleteSQL,function(err, data){
-
-        db.query("select * from employees",function(err, data){
-            res.render("employeelist.ejs",{employees:data})
-            res.end()
-        })
-    })
-})
-
 
 server.listen(3000, ()=>{
-    console.log('Server is running on PORT 3000');
-    console.log('ctrl+C to close');
+    console.log('Server is running...');
 })
