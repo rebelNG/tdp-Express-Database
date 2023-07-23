2a58d6cf209756388bc7de271ccdf099669c2c90
Your Name <you@example.com>
2023-07-23 17:36:30 +0100

    Fixed error

--- Project1/server.js
+++ Project1/server.js
@@ -17,14 +17,38 @@ server.get("/", (req,res)=>{
     res.end()
 })
 
+server.get("/allemployeelist", (req, res)=>{
+    db.query("Select * from employee",(err, data)=>{
+        res.render("employeelist.ejs",{employee:data})
+        res.end()
+    })
+})
+
 server.get("/departments", (req,res)=>{
     
     db.query("Select distinct department from employee",(err, data)=>{
-        res.render("departments.ejs")
+        res.render("departments.ejs",{deptName:data})
         res.end()
     })
+})
 
+server.get("/employeeDepartment/:dept", (req,res)=>{
     
+    let sql1 = "Select * from employee where department='"+req.params.dept+"'"
+    db.query(sql1,(err, data)=>{
+        res.render("employeelist.ejs",{employee:data})
+        res.end()
+    })
+})
+
+server.get("/deleteemployee/:empId", (req, res)=>{
+    let deleteSQL = "delete from employee where empId="+req.params.empId
+    db.query(deleteSQL, (err, data)=>{
+        db.query("Select * from employee",(err, data)=>{
+            res.render("employeelist.ejs",{employee:data})
+            res.end()
+        })
+    })
 })
 
 server.listen(3000, ()=>{
